0a915a90427f57070246a46c9945f4c45e70dd70
Thiago Paixão <tpaxadev@gmail.com>
2016-04-08 03:58:25 -0300

    fixing some test stuffs

--- spec/views/todos/index.html.erb_spec.rb
+++ spec/views/todos/index.html.erb_spec.rb
@@ -4,22 +4,24 @@ RSpec.describe "todos/index", type: :view do
   before(:each) do
     assign(:todos, [
       Todo.create!(
-        :title => "Title",
-        :private => false,
-        :completed => false
+        title: "Title",
+        tasks_attributes: [{body: 'something'}],
+        private: false,
+        completed: false
       ),
       Todo.create!(
-        :title => "Title",
-        :private => false,
-        :completed => false
+        title: "Title",
+        tasks_attributes: [{body: 'something'}],
+        private: false,
+        completed: false
       )
     ])
   end
 
   it "renders a list of todos" do
     render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
-    assert_select "tr>td", :text => false.to_s, :count => 2
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: false.to_s, count: 4
+    assert_select "tr>td", text: false.to_s, count: 4
   end
 end
