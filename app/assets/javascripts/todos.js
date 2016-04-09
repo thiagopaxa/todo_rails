7dcf10464d4513a34904ba06621d28d0d2d23331
Thiago Paixão <tpaxadev@gmail.com>
2016-04-09 04:19:46 -0300

    sign_out working nicely

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,2 +1,30 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+;(function(root){
+  'use strict';
+  root.Todo = window.Todo || {};
+
+  Todo.signUserOut = function() {
+    $.ajax({
+      url: '/users/sign_out',
+      type: 'DELETE',
+
+      success: function(){
+        window.location.reload();
+      },
+
+      error: function(data) {
+        console.error(data);
+      }
+    });
+  };
+
+  Todo.bindings = function(){
+
+    $('.sign-out-btn').on('click', Todo.signUserOut);
+  };
+
+})(window);
+
+
+$(function(){
+  Todo.bindings();
+});
