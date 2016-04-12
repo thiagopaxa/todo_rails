e45459a62f5392190096b132ae453a1b6b78b414
Thiago Paixão <tpaxadev@gmail.com>
2016-04-12 06:19:25 -0300

    path redirect change

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,7 +31,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to todos_url, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -47,7 +47,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to todos_url, notice: 'Todo was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
